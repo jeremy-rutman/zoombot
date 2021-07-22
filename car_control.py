2bca43614ffc48108fbbccf6e876c920bce493fd
jeremy-rutman <jeremy.rutman@gmail.com>
2021-07-22 12:52:21 +0300

    oops

--- car_control.py
+++ car_control.py
@@ -1,15 +1,34 @@
 # this is the easy part and hence left to last
 # given input command, turn on the corresponding GPIO pin
 
-# import RPi.GPIO as rpi
-# import RPi
-#rpi.VERSION
-
 import request
 import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+FWD_PIN = 27
+REV_PIN = 28
+LEFT_PIN = 39
+RIGHT_PIN = 40
+LIGHTS_PIN = 41
+all_pins = [FWD_PIN,REV_PIN,LEFT_PIN,RIGHT_PIN]
+for channel in all_pins:
+    GPIO.setup(channel, GPIO.IN)
+    GPIO.output(channel, GPIO.LOW)
+
 
 def do_command(command):
     print(f'doing command {command}')
+    if command == 'REV':
+        GPIO.output(REV_PIN,GPIO.HIGH)
+        for i in range(3):
+            sleep(0.2)
+            GPIO.output(LIGHTS_PIN,GPIO.HIGH)
+            sleep(0.2)
+            GPIO.output(LIGHTS_PIN,GPIO.LOW)
+        GPIO.output(REV_PIN,GPIO.LOW)
+    if command == 'FWD':
+        #  YOUR CODE GOES HERE!!!
+
 
 last_executed_command=(0,'F')
 while(1):
